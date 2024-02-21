135866ec8cc081158d14b03848a6eb765d8e9ec2
GMDG <gmdg141995@gmail.com>
2024-02-21 19:15:58 +0100

    Created basic AI classes

---
+++ CyberpunkDemo/Source/CyberpunkDemo/Public/Enemies/BasicEnemyController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BasicEnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CYBERPUNKDEMO_API ABasicEnemyController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
