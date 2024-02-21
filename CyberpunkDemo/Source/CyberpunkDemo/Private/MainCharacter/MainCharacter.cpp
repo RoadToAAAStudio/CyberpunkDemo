33a14c10291d37c2ecdd2043bfd4f948acfd1cba
JackAlone96 <giacalone.giovanni96@gmail.com>
2024-02-21 22:34:40 +0100

    Implementation of the base logic for the character, character movement and player controller

--- CyberpunkDemo/Source/CyberpunkDemo/Private/MainCharacter/MainCharacter.cpp
+++ CyberpunkDemo/Source/CyberpunkDemo/Private/MainCharacter/MainCharacter.cpp
@@ -2,20 +2,57 @@
 
 
 #include "MainCharacter/MainCharacter.h"
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "CyberpunkDemo/CyberpunkDemoCharacter.h"
+#include "Engine/LocalPlayer.h"
 
-// Sets default values
+// Sets default values and create the components
 AMainCharacter::AMainCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	//PrimaryActorTick.bCanEverTick = true;
 
+	// Set the booleans for the movement system to false
+	bIsCrouching = false;
+	bIsRunning = false;
+
+	// Set size for a collision capsule [!]
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.f);
+
+	// Create a camera component [!]
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f));
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	// Create a mesh component [!]
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh"));
+	Mesh1P->SetOnlyOwnerSee(true);
+	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+	Mesh1P->CastShadow = false;
+	Mesh1P->bCastDynamicShadow = false;
+	Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
 }
 
 // Called when the game starts or when spawned
 void AMainCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// Add Input Mapping Context [!]
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
 }
 
 // Called every frame
@@ -25,10 +62,50 @@ void AMainCharacter::Tick(float DeltaTime)
 
 }
 
-// Called to bind functionality to input
+// Called to bind functionality to input [!]
 void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// Set up action bindings
+	//Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Jumping
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
+		//Moving
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AMainCharacter::Move);
+
+		// Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AMainCharacter::Look);
+	}
 }
+
+// Called to apply the direction to the movement [!]
+void AMainCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2d MovementVector = Value.Get<FVector2d>();
+
+	if (Controller != nullptr)
+	{
+		// add movement
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+// Called to add the input rotation to the controller [!]
+void AMainCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2d LookAxisVector = Value.Get<FVector2d>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
+
+
 
--- CyberpunkDemo/Source/CyberpunkDemo/Public/MainCharacter/MainCharacter.h
+++ CyberpunkDemo/Source/CyberpunkDemo/Public/MainCharacter/MainCharacter.h
@@ -1,29 +1,87 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+// Includes
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
 #include "MainCharacter.generated.h"
 
-UCLASS()
+// Forward declarations
+class UInputComponent;
+class USkeletalMeshComponent;
+class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
+UCLASS(config=Game)
 class CYBERPUNKDEMO_API AMainCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Movement")
+	bool bIsRunning;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Movement")
+	bool bIsCrouching;
+
+private:
+	
+	/** Pawn mesh: 1st person view (arms; seen only by self) [!] */
+    	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+    	USkeletalMeshComponent* Mesh1P;
+    
+    	/** First person camera [!] */
+    	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    	UCameraComponent* FirstPersonCameraComponent;
+    
+    	/** MappingContext [!] */
+    	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+    	UInputMappingContext* DefaultMappingContext;
+    
+    	/** Jump Input Action [!] */
+    	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+    	UInputAction* JumpAction;
+    
+    	/** Move Input Action [!] */
+    	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+    	UInputAction* MoveAction;
+
+		/** Look Input Action [!] */
+		UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	    UInputAction* LookAction;
+
+public:
 	// Sets default values for this character's properties
 	AMainCharacter();
+
+	/** Returns Mesh1P sub-object [!] **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent sub-object [!] **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	/** Called for movement input [!] */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input [!] */
+	void Look(const FInputActionValue& Value);
+
+	// APawn interface [!]
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	// End of APawn interface
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// // Called to bind functionality to input
+	// virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
