5dc2091e440906057a958cf9efe87326ceeab81e
GMDG <gmdg141995@gmail.com>
2024-02-23 18:58:39 +0100

    Added a AI debug map and widget

--- CyberpunkDemo/Source/CyberpunkDemo/CyberpunkDemo.Build.cs
+++ CyberpunkDemo/Source/CyberpunkDemo/CyberpunkDemo.Build.cs
@@ -8,6 +8,6 @@ public class CyberpunkDemo : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayStateTreeModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayStateTreeModule", "GameplayTags" });
 	}
 }
